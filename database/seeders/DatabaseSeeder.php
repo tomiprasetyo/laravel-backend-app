cadac050dfd3e00b9229e01de9292e286c1f6403
Tomi Prasetyo <tomi.prasetyo95@gmail.com>
2023-08-26 23:46:06 +0700

    midtrans integration, auth register, logout, feature orders & upload image

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProductSeeder::class,
+            OrderSeeder::class,
+            OrderItemSeeder::class
         ]);
     }
 }
